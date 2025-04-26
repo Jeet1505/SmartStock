f7cad99454a7cb4254a3a854770302964245543f
Jeet1505 <23ce113@charusat.edu.in>
2025-04-26 12:04:07 +0530

    host page UI

--- smartstock_backend/smartstock_backend/urls.py
+++ smartstock_backend/smartstock_backend/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse("""
+        <html>
+            <head>
+                <title>SmartStock</title>
+            </head>
+            <body style="background-color: #f5f7fa; text-align: center; padding-top: 100px; font-family: Arial, sans-serif;">
+                <h1 style="color: #333;">🚀 Welcome to <span style="color: #007bff;">SmartStock API</span> 👋</h1>
+                <p style="font-size: 18px; color: #666;">Your gateway to smart stock screening and analysis.</p>
+            </body>
+        </html>
+    """)),
     path('api/', include('authapp.urls')),  # adjust if your app has a different name
 ]
